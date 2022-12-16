bbdd9f9c4e588f37d1c94c5a1ead8d517f34c8c0
arm-out <arminsuraj@gmail.com>
2022-12-15 20:32:13 -0800

    day 7

---
+++ day07B.py
@@ -0,0 +1,77 @@
+f = open("inputs/day07.txt", "r")
+input = f.read().splitlines()
+
+class Directory:
+    def __init__(self, path):
+        self.path = path
+        self.size = -1
+        self.childFiles = []
+        self.childDirectories = []
+
+class File:
+    def __init__(self, path, size):
+        self.path = path
+        self.size = size
+
+allDirectories = {'/': Directory('/')}
+allFiles = {}
+pwd = []
+
+def createFileTree():
+    # Parse commands
+    for line in input:
+        line = line.split(" ")
+        # command
+        if line[0] == '$':
+            if line[1] == 'ls':
+                continue
+            else:
+                target = line[2]
+                if target =='/':
+                    pwd = ['/']
+                elif target == '..':
+                    pwd.pop()
+                else:
+                    pwd += [target + '/']
+        else:
+            parent = allDirectories[''.join(pwd)]
+            if line[0] == 'dir':
+                curPath = ''.join(pwd) + line[1] + '/'
+                if (curPath not in allDirectories):
+                    allDirectories[curPath] = Directory(curPath)
+                parent.childDirectories.append(curPath)
+            else:
+                curPath = ''.join(pwd) + line[1]
+                if (curPath not in allFiles):
+                    allFiles[curPath] = File(curPath, int(line[0]))
+                parent.childFiles.append(curPath)
+
+def dirSize(directory):
+    if (directory.size >= 0):
+        return directory.size
+    else:
+        totalFileSize = 0
+        for file in directory.childFiles:
+            totalFileSize += allFiles[file].size
+
+        totalDirSize = 0
+        for cDir in directory.childDirectories:
+            totalDirSize += dirSize(allDirectories[cDir])
+        directory.size = totalFileSize + totalDirSize
+        return directory.size
+
+createFileTree()
+dirSize(allDirectories['/'])
+
+totalDiskSpace = 70000000
+minUnusedSpace = 30000000
+totalUsedSpace = allDirectories['/'].size
+spaceNeeded = minUnusedSpace - (totalDiskSpace - totalUsedSpace)
+
+deleteSize = 30000000
+for dirPath in allDirectories:
+    if allDirectories[dirPath].size >= spaceNeeded and allDirectories[dirPath].size < deleteSize:
+        deleteSize = allDirectories[dirPath].size
+
+print(deleteSize)
+        
