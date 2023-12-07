806913f522640ccace22c11decdf889a398c5281
arm-out <arminsuraj@gmail.com>
2023-12-06 20:20:43 -0800

    better input and test utils

--- 2023/utils.py
+++ 2023/utils.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 
 def getInput(day):
@@ -6,3 +7,8 @@ def getInput(day):
     load_dotenv()
     if not os.path.isfile(f"inputs/day{day_pad}.txt"):
         os.system(f"curl https://adventofcode.com/2023/day/{day}/input --cookie session={os.environ['AOC_SESSION']} > inputs/day{day_pad}.txt")
+
+    if len(sys.argv) > 1:
+        return open("inputs/test.txt", "r").read().splitlines()
+    else:
+        return open(f"inputs/day{day_pad}.txt", "r").read().splitlines()
